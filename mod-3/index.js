f2e2b8ea1680c1ae99cdb497b5ad29b8e911cd0c
Finn Thye <piqueen@gmail.com>
2020-12-30 08:50:24 -0700

    addEventListener counter

--- mod-3/index.js
+++ mod-3/index.js
@@ -1,1 +1,9 @@
-console.log("Hello from JavaScript")
+var myButton = document.getElementById('myButton')
+console.log(myButton)
+var myTitle = document.getElementById('title')
+var counter = 5
+
+myButton.addEventListener('click', function() {
+  counter += 5
+  myButton.textContent = counter
+})
