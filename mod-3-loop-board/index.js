a74a2561d0e04e70ef01936ef782790176a1e369
Finn Thye <piqueen@gmail.com>
2020-12-30 17:08:10 -0700

    prep to color a board by looping

---
+++ mod-3-loop-board/index.js
@@ -0,0 +1,8 @@
+const squares = Array.from(document.querySelectorAll('.grid div'))
+
+//Challenge: We use what we have learnt in the previous three lessons in order 
+//to visually colour a game board in our browser. At the moment the board is empty. 
+//Please colour the squares, similar to like you would on a chess board using the 
+//for loop.
+
+//Don't forget to share your code in the #share-your-code channel on discord.
