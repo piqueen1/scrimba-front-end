44b93a465676d4adcd26e9f984e27d2d81f48134
Finn Thye <piqueen@gmail.com>
2021-01-07 11:21:57 -0700

    List challenge completed

--- mod-3-list/index.js
+++ mod-3-list/index.js
@@ -3,21 +3,27 @@ const upButton = document.querySelector('#up')
 const buyListDisplay = document.querySelector('#first-list')
 const fridgeListDisplay = document.querySelector('#second-list')
 
-const buyList = ['almond milk', 'avocado', 'cheese', 'halloumi' ]
-const fridge = []
-
-//Challenge: Please fill the fridge with 5 items of your choice. 
-
+let buyList = ['almond milk', 'avocado', 'cheese', 'halloumi' ]
+let fridge = ['cottage cheese', 'strawberries', 'coconut milk', 'eggs', 'produce']
 
 //Chellenge 2: You have bought some almond milk. Please remove it from the
 //buyList and add it to the items in fridge.
 
+function firstFromBuyToFridge() {
+    const movedItem = buyList.shift();
+    fridge.push(movedItem)
+}
+firstFromBuyToFridge(buyList)
 
 //BONUS 1:Write a function that will remove an item from the fridge, and put it in the
 //buyList, on the press of the moveUp button.
 
-function moveUp(){
-    //your code
+function moveUp() {
+    console.log('move up button pressed')
+    let lastInFridge = fridge.pop();
+    buyList.push(lastInFridge)
+    buyListDisplay.innerHTML = buyList
+    fridgeListDisplay.innerHTML = fridge
 }
 
 upButton.addEventListener('click', moveUp)
@@ -26,7 +32,11 @@ upButton.addEventListener('click', moveUp)
 //and put it in the fridge.
 
 function moveDown(){
-    //your code
+    console.log('move down button pressed')
+    let lastOnBuy = buyList.pop();
+    fridge.push(lastOnBuy)
+    buyListDisplay.innerHTML = buyList
+    fridgeListDisplay.innerHTML = fridge
 }
 
 downButton.addEventListener('click', moveDown)
