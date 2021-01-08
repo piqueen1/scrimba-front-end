a81f6504c237a591b192526d5d73ad30d9932f0e
Finn Thye <piqueen@gmail.com>
2021-01-08 14:01:42 -0700

    Determine if snake hit walls

--- mod-4-snake/index.js
+++ mod-4-snake/index.js
@@ -20,20 +20,30 @@ createGrid()
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
-  //remove last element of current snake
-  const tail = currentSnake.pop()
-  //remove styling from last element
-  squares[tail].classList.remove('snake')
-  //add square in direction we are heading
-  currentSnake.unshift(currentSnake[0] + direction)
-  //add styling to that square
-  squares[currentSnake[0]].classList.add('snake')
+if (
+  (currentSnake[0] % width === 0 && direction === -1) ||
+  (currentSnake[0] % width === width - 1 && direction === 1) ||
+  (currentSnake[0] + width >= width*width && direction === width) ||
+  (currentSnake[0] % width < 0 && direction === -width) ||
+  squares[currentSnake[0]] + direction].classList.contains('snake')
+  ) {
+    return clearInterval(timerId)
+  } else {
+    //remove last element of current snake
+    const tail = currentSnake.pop()
+    //remove styling from last element
+    squares[tail].classList.remove('snake')
+    //add square in direction we are heading
+    currentSnake.unshift(currentSnake[0] + direction)
+    //add styling to that square
+    squares[currentSnake[0]].classList.add('snake')
+  }
 }
 
 move()
 
 let timerID = setInterval(move,1000)
-//clearInterval(timerID)
+clearInterval(timerID)
 //keycodes
 // 37 is left arrow
 // 38 is up arrow
@@ -46,13 +56,13 @@ function control(e) {
     direction = -1
   } else if (e.keyCode === 38) {
     console.log('up pressed')
-    direction = -10
+    direction = -width
   } else if (e.keyCode === 39) {
     console.log('right pressed')
     direction = 1
   } else if (e.keyCode === 40) {
     console.log('down pressed')
-    direction = 10
+    direction = width
   }
 }
 
