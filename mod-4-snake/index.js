9d2da367df063658ce37d9f74036959c92a2b06b
Finn Thye <piqueen@gmail.com>
2021-01-07 20:49:07 -0700

    Create grid board

--- mod-4-snake/index.js
+++ mod-4-snake/index.js
@@ -1,2 +1,16 @@
 // javascript
-document.querySelector('h1').innerHTML = 'Snake Game'
+const grid = document.querySelector('.grid')
+const startButton = document.querySelector('#start')
+const score = document.getElementById('score')
+const squares = []
+
+function createGrid() {
+  for (let i = 0; i<100; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    grid.appendChild(square)
+    squares.push(square)
+  }
+}
+
+createGrid()
