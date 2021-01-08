d7725a79aad90eae209577e6a9424334dcfa1a85
Finn Thye <piqueen@gmail.com>
2021-01-08 15:05:33 -0700

    Complete eat apple function

--- mod-4-snake/index.js
+++ mod-4-snake/index.js
@@ -1,10 +1,15 @@
 // javascript
 const grid = document.querySelector('.grid')
 const startButton = document.querySelector('#start')
-const score = document.getElementById('score')
+const scoreDisplay = document.getElementById('score')
+const width = 10
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
+let appleIndex = 0
+let score = 0
+let intervalTime = 1000
+let speed = 0.9
 
 function createGrid() {
   for (let i = 0; i<100; i++) {
@@ -20,15 +25,15 @@ createGrid()
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
-if (
-  (currentSnake[0] % width === 0 && direction === -1) ||
-  (currentSnake[0] % width === width - 1 && direction === 1) ||
-  (currentSnake[0] + width >= width*width && direction === width) ||
-  (currentSnake[0] % width < 0 && direction === -width) ||
-  squares[currentSnake[0]] + direction].classList.contains('snake')
-  ) {
+  if (
+    (currentSnake[0] % width === 0 && direction === -1) ||
+    (currentSnake[0] % width === width - 1 && direction === 1) ||
+    (currentSnake[0] + width >= width*width && direction === width) ||
+    (currentSnake[0] % width < 0 && direction === -width) ||
+    squares[currentSnake[0] + direction].classList.contains('snake')
+    ) 
     return clearInterval(timerId)
-  } else {
+    
     //remove last element of current snake
     const tail = currentSnake.pop()
     //remove styling from last element
@@ -36,14 +41,43 @@ if (
     //add square in direction we are heading
     currentSnake.unshift(currentSnake[0] + direction)
     //add styling to that square
+    //deal with snake head getting the apple
+    if (squares[currentSnake[0]].classList.contains('apple')) {
+      //remove class of apple
+      squares[currentSnake[0]].classList.remove('apple')
+      //grow snake by adding class of snake to it
+      squares[currentSnake[0]].classList.add('snake')
+      squares[tail].classList.add('snake')
+      //grow our snake array
+      currentSnake.push(tail)
+      //generate a new apple
+      generateApple()
+      //add one to the score
+      score++
+      //display our score
+      scoreDisplay.textContent = score
+      //speed up our snake
+      clearInterval(timerId)
+      console.log(intervalTime)
+      intervalTime = intervalTime * speed
+      console.log(intervalTime)
+      timerId = setInterval(move, intervalTime)
+    }
     squares[currentSnake[0]].classList.add('snake')
   }
-}
+  
+  let timerId = setInterval(move,intervalTime)
+  move()
+  
+  function generateApple() {
+    do {
+      //generate a random number
+      appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple')
+  }
 
-move()
-
-let timerID = setInterval(move,1000)
-clearInterval(timerID)
+generateApple()
 //keycodes
 // 37 is left arrow
 // 38 is up arrow
