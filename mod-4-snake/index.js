903b07cce28029f24bac656b81a76f51982d6919
Finn Thye <piqueen@gmail.com>
2021-01-07 21:06:35 -0700

    Complete move function

--- mod-4-snake/index.js
+++ mod-4-snake/index.js
@@ -2,7 +2,9 @@
 const grid = document.querySelector('.grid')
 const startButton = document.querySelector('#start')
 const score = document.getElementById('score')
-const squares = []
+let squares = []
+let currentSnake = [2,1,0]
+let direction = 1
 
 function createGrid() {
   for (let i = 0; i<100; i++) {
@@ -14,3 +16,19 @@ function createGrid() {
 }
 
 createGrid()
+
+currentSnake.forEach(index => squares[index].classList.add('snake'))
+
+function move() {
+  //remove last element of current snake
+  const tail = currentSnake.pop()
+  //remove styling from last element
+  squares[tail].classList.remove('snake')
+  //add square in direction we are heading
+  currentSnake.unshift(currentSnake[0] + direction)
+  //add styling to that square
+  squares[currentSnake[0]].classList.add('snake')
+}
+
+move()
+move()
