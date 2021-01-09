ac31eafe8b239438b91048975490d38dcd32b623
Finn Thye <piqueen@gmail.com>
2021-01-09 07:31:21 -0700

    Hook up add to end and beginning buttons

--- mod-3-emojis/index.js
+++ mod-3-emojis/index.js
@@ -1,3 +1,33 @@
-// 1. Loop through the myEmojis array and log each of them out to the console
-
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const emojiContainer = document.getElementById("emoji-container")
+
+function renderEmojis() {
+  emojiContainer.innerHTML = ''
+  for (let i = 0; i < myEmojis.length; i++) {
+      const newSpan = document.createElement('span')
+      newSpan.textContent = myEmojis[i]
+      emojiContainer.append(newSpan)
+  }
+}
+
+const pushBtn = document.getElementById('push-btn')
+pushBtn.addEventListener('click', function() {
+  const emojiInput = document.getElementById('emoji-input')
+  if (emojiInput.value) {
+    myEmojis.push(emojiInput.value)
+    console.log(myEmojis)
+    emojiInput.value = ''
+    renderEmojis() 
+  }
+})
+
+const unshiftBtn = document.getElementById('unshift-btn')
+unshiftBtn.addEventListener('click', function() {
+  const emojiInput = document.getElementById('emoji-input')
+  if (emojiInput.value) {
+    myEmojis.unshift(emojiInput.value)
+    console.log(myEmojis)
+    emojiInput.value = ''
+    renderEmojis() 
+  }
+})
