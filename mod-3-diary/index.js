2bb429f938763f0d1954f66d7d068167cc0e1e81
Finn Thye <piqueen@gmail.com>
2021-01-10 07:38:44 -0700

    Hook single entry div up to submit button

--- mod-3-diary/index.js
+++ mod-3-diary/index.js
@@ -1,8 +1,14 @@
 const entryForm = document.getElementById('entryForm')
+const entriesSection = document.getElementById('entries')
+const entryTextbox = document.getElementsByClassName('entry-textbox')
+const singleEntry = document.createElement('div')
 
 function addEntryToDom(event) {
   event.preventDefault();
+  singleEntry.innerText = entryTextbox[0].value;
+  singleEntry.className = 'single-entry';
+  entriesSection.appendChild(singleEntry);
 }
 
 entryForm.addEventListener('submit', addEntryToDom)
-})
+
