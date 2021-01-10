00371a7fd059a2d7a7df5f47afe41313008cf9b6
Finn Thye <piqueen@gmail.com>
2021-01-10 11:25:29 -0700

    Create single entry nav buttons

--- mod-3-diary/index.js
+++ mod-3-diary/index.js
@@ -1,13 +1,22 @@
-const entryForm = document.getElementById('entryForm')
-const entriesSection = document.getElementById('entries')
-const entryTextbox = document.getElementsByClassName('entry-textbox')
-const singleEntry = document.createElement('div')
+const entryForm = document.getElementById('entryForm');
+const entriesSection = document.querySelector('#entries');
+const entryTextbox = document.querySelector('.entry-textbox');
+const singleEntry = document.createElement('div');
+const entriesNav = document.querySelector('.entries-nav');
+let entryNumber = 1
 
 function addEntryToDom(event) {
   event.preventDefault();
-  singleEntry.innerText = entryTextbox[0].value;
+  singleEntry.innerText = entryTextbox.value;
   singleEntry.className = 'single-entry';
+  singleEntry.style.display = 'none';
   entriesSection.appendChild(singleEntry);
+  entryTextbox.value = '';
+
+  const displayEntryButton = document.createElement('button')
+  displayEntryButton.className = 'display-entry-button'
+  displayEntryButton.innerText = entryNumber++
+  entriesNav.appendChild(displayEntryButton)
 }
 
 entryForm.addEventListener('submit', addEntryToDom)
