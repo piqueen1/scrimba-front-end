5eb8167df20357e94b1cb52f65bed8f4c97c456d
Finn Thye <piqueen@gmail.com>
2021-01-11 19:44:23 -0700

    Write variables for logic and dom nodes

--- mod-4-dice/index.js
+++ mod-4-dice/index.js
@@ -1,1 +1,13 @@
 // Create variables for the game state
+let score1 = 0
+let score2 = 0
+let player1turn = true
+
+//Create variables for dom nodes (all changing elements)
+const message = document.getElementById('message')
+const scoreDisplay1 = document.getElementById('player1Scoreboard')
+const scoreDisplay2 = document.getElementById('player2Scoreboard')
+const player1Dice = document.getElementById('player1Dice')
+const player2Dice = document.getElementById('player2Dice')
+const rollBtn = document.getElementById('rollBtn')
+const resetBtn = document.getElementById('resetBtn')
