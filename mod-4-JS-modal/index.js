94a1b792d2324137641ef2fd59b83452f3b72190
Finn Thye <piqueen@gmail.com>
2021-01-15 12:53:19 -0700

    Set up JS modal challenge

---
+++ mod-4-JS-modal/index.js
@@ -0,0 +1,3 @@
+// we need an event listener just attaches to an element (like a light switch) - in this case a DOM element, our button(s)!
+// DOM elements = anything you see on the page on the document (divs, buttons, tables, links--any HTML element!divs, buttons, tables, links--any HTML element!)
+// you can switch out "click" or be other types of DOM events (keydown, mouseover), any sort of event that a user can do, you can capture user actions using event listeners!
