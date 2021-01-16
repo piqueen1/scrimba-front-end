23804b460707ddc3a8b7c3fc777ccb1c00b24b63
Finn Thye <piqueen@gmail.com>
2021-01-16 15:57:10 -0700

    Complete searchbar functionality

--- mod-4-JS-search/index.js
+++ mod-4-JS-search/index.js
@@ -1,12 +1,13 @@
-/*
-    Task: Capture the user's search query
-    1. Add an event listener for 'keyup'
-    2. Capture the event in a 'let' called 'searchQuery' - storing the value
-    3. Make sure the captured value is lowercase
-    4. Log to the console to demonstrate it works
-*/
-
 document.getElementById('searchInput').addEventListener('keyup', function(event) {
     let searchQuery = event.target.value.toLowerCase()
-    console.log(searchQuery)
+    let allNamesDOMCollection = document.getElementsByClassName('name')
+
+    for (let i=0; i<allNamesDOMCollection.length; i++) {
+        currentName = allNamesDOMCollection[i].textContent.toLowerCase()
+        if (searchQuery.includes(currentName)) {
+            allNamesDOMCollection[i].style.display = 'block';
+        } else {
+            allNamesDOMCollection[i].style.display = 'none'
+        }
+    }
 })
