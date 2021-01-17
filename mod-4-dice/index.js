fefa0e09a3708e0212ebdfa742e4b372990cb75d
Finn Thye <piqueen@gmail.com>
2021-01-16 20:34:53 -0700

    Mysteriously screwed everything up

--- mod-4-dice/index.js
+++ mod-4-dice/index.js
@@ -9,6 +9,8 @@ const scoreDisplay1 = document.getElementById('player1Scoreboard')
 const scoreDisplay2 = document.getElementById('player2Scoreboard')
 const player1Dice = document.getElementById('player1Dice')
 const player2Dice = document.getElementById('player2Dice')
+const equation1 = document.getElementById('equation1')
+const equation2 = document.getElementById('equation2')
 const rollBtn = document.getElementById('rollBtn')
 const resetBtn = document.getElementById('resetBtn')
 
@@ -30,28 +32,30 @@ function reset() {
   player2Dice.textContent = '-'
   resetBtn.style.display = 'none'
   rollBtn.style.display = 'block'
+  equation1.textContent = ''
+  equation2.textContent = ''
 }
 
 rollBtn.addEventListener('click', () => {
   const roll = Math.floor(Math.random() * 6) + 1
   
   if (player1Turn) {
+    equation1.textContent = `${score1} + ${roll} = ${score1 += roll}`
     player1Dice.textContent = roll
-    score1 += roll
     scoreDisplay1.textContent = score1
     message.textContent = "Player Two's Turn"
     player1Dice.classList.remove('active')
     player2Dice.classList.add('active')
   } else {
+    equation2.textContent = `${score2} + ${roll} = ${score2 += roll}`
     player2Dice.textContent = roll
-    score2 += roll
     scoreDisplay2.textContent = score2
     message.textContent = "Player One's Turn"
     player2Dice.classList.remove('active')
     player1Dice.classList.add('active')
   }
   player1Turn = !player1Turn
-
+  
   //check for winner
   if (score1 >= 20) {
     message.textContent = "Player One WINS!  WOOT!"
