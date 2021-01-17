358c8d4c8a46f973d9c5fc34cf04aa550b956730
Finn Thye <piqueen@gmail.com>
2021-01-17 13:45:07 -0700

    Add next slide functionality

--- mod-4-JS-carousel/index.js
+++ mod-4-JS-carousel/index.js
@@ -2,16 +2,23 @@ const slides = document.getElementsByClassName('carousel-item')
 let slidePosition = 0;
 const totalSlides = slides.length;
 
+document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide)
+document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide)
+
 function moveToPrevSlide() {
   slidePosition -= 1
 }
 
 function moveToNextSlide() {
-  slidePosition += 1
+  slides[slidePosition].classList.remove('carousel-item-visible')
+  
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0
+  } else {
+    slidePosition += 1
+  }
+  slides[slidePosition].classList.add('carousel-item-visible')
+
 }
-
-document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide)
-document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide)
-
 
 console.log(totalSlides)
