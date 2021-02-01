159d02ea2ba62a5c2d4464a5981d482b51709fd4
Finn Thye <piqueen@gmail.com>
2021-01-31 17:23:40 -0700

    Set up promises challenge

---
+++ mod-6-challenge-promises/index.js
@@ -0,0 +1,9 @@
+/**
+ * Promises - Challenge
+ * Create a promise that returns some user data and throws an error when not found.
+ * Log the user data when listening to the promise as well as log the error.
+ * 
+ * Docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+*/
+
+
