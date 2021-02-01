85e50389ec4c5938a6a161e0abe1748e2b1db211
Finn Thye <piqueen@gmail.com>
2021-01-31 17:33:40 -0700

    My solution to promises challenge

--- mod-6-challenge-promises/index.js
+++ mod-6-challenge-promises/index.js
@@ -6,4 +6,19 @@
  * Docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
 */
 
+const getUserData = () => {
+  return new Promise( (resolve,reject) => {
+    setTimeout( () => {
+      const error = false;
+      if( error ) {
+        reject("No user data was found")
+      } else {
+        resolve("Here is your user data")
+      }
+    }, 3000)
+  })
+}
 
+getUserData()
+.then( (success) => console.log(success) )
+.catch( (error) => console.log(error) );
