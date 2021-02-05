2d8bedc7252e3bb91f52933c08d9fa6d11b1ffad
Finn Thye <piqueen@gmail.com>
2021-02-05 09:09:02 -0700

    Add logic  for displaying setup, saving punchline and toggling hidden buttons

--- mod-7-jokes/index.js
+++ mod-7-jokes/index.js
@@ -2,16 +2,24 @@ const setupDiv = document.getElementById('setup')
 const punchlineDiv = document.getElementById('punchline')
 const punchlineBtn = document.getElementById('punchlineBtn')
 const newJokeBtn = document.getElementById('newJokeBtn')
+let punchline = ""
 
 async function getJoke() {
   const jokePromise = await fetch("https://official-joke-api.appspot.com/jokes/programming/random")
 
-  if (jokePromise.ok) {
-    const joke = await jokePromise.json()
-    console.log(joke)
-  } else {
-    console.error(`Error status: ${err}`)
-  }
+
+  const joke = await jokePromise.json()
+
+  setupDiv.innerHTML = joke[0].setup
+  punchline = joke[0].punchline
+  
+  punchlineBtn.classList.toggle('hidden')
+  newJokeBtn.classList.toggle('hidden')
+
 }
+
+    // Create a global variable called punchline which will store the current punchline and will be updated with each new joke
+    // Assign the current jokes punchline to the punchline variable.
+
 
 getJoke()
