88c561287610a85aa3d3040f980eac0d1ebb0268
Finn Thye <piqueen@gmail.com>
2021-02-19 10:42:27 -0700

    Set up promise challenge

---
+++ mod-7-challenge-promise/index.js
@@ -0,0 +1,19 @@
+// create a promise 
+// wait for 4 seconds
+// resolve or reject based on randomBool
+// false - reject 'hero is on vacation'
+// true - resolve 'batman is here'
+
+
+const randomBool = Boolean(Math.round(Math.random())) // returns either 0 or 1 
+
+function 
+
+(async function() {
+    try{
+        
+        
+    }catch(err){
+        console.log(err)
+    }
+})();
