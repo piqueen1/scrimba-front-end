1babb86b54dd5a324e10f95f349b29bb2aa05218
Finn Thye <piqueen@gmail.com>
2021-02-25 14:37:10 -0700

    Submit form data with promise chaining

---
+++ mod-7-advanced-submit-form/index.js
@@ -0,0 +1,19 @@
+const userForm = document.getElementById('userForm');
+
+userForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const formData = new FormData(this);
+    const options = {
+        method: 'POST',
+        body: formData
+    };
+    
+    // Write the Fetch statement using https://httpbin.org/post along with the options. Then console log the JSON response. 
+    
+    fetch('https://httpbin.org/post', options)
+        .then(resp => resp.json())
+        .then(json => console.log(json.form))
+        .catch(err => console.error(err));
+    
+});
