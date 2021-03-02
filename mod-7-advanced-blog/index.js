d1eebea94fb44129f44373f15572f43e9d9f1b10
Finn Thye <piqueen@gmail.com>
2021-03-02 14:42:28 -0700

    Halfway through lesson

--- mod-7-advanced-blog/index.js
+++ mod-7-advanced-blog/index.js
@@ -1,13 +1,19 @@
 // Use Fetch with this URL, https://jsonplaceholder.typicode.com/posts/1, to get a single blog post and render the blog title and body to the page.
 
 async function getPost() {
-  const postPromise = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const [postPromise, userPromise] = await Promise.all([
+    fetch("https://jsonplaceholder.typicode.com/posts"),
+    fetch("https://jsonplaceholder.typicode.com/users")
+  ]);
 
   const posts = await postPromise.json();
+  const users = await userPromise.json();
   
+  console.log(users[0])
 
   const html = `
   <h3>${posts[0].title}</h3>
+  <h5>${users.filter(user => user.id === posts[0].userId)[0].name}</h5>
   <p>${posts[0].body}</p>
   `;
 
