2f36694104c66bdc78ade11bbef729fbbf15bceb
Finn Thye <piqueen@gmail.com>
2021-03-02 14:25:29 -0700

    Set up placeholder blog json

--- mod-7-advanced-blog/index.js
+++ mod-7-advanced-blog/index.js
@@ -1,1 +1,17 @@
 // Use Fetch with this URL, https://jsonplaceholder.typicode.com/posts/1, to get a single blog post and render the blog title and body to the page.
+
+async function getPost() {
+  const postPromise = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  const posts = await postPromise.json();
+  
+
+  const html = `
+  <h3>${posts[0].title}</h3>
+  <p>${posts[0].body}</p>
+  `;
+
+  document.body.innerHTML = html;
+}
+
+getPost();
