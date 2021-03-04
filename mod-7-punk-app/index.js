963d729fa4ee6de52ebb4da740351deaa5d08096
Finn Thye <piqueen@gmail.com>
2021-03-04 13:43:13 -0700

    Start js fetch / displaying fetched data

--- mod-7-punk-app/index.js
+++ mod-7-punk-app/index.js
@@ -1,1 +1,11 @@
-// javascript
+// variables
+const urlBase = "https://api.punkapi.com/v2/beers"
+
+async function getBeers() {
+  const promise = await fetch(urlBase)
+  const json = await promise.json()
+
+  console.log(json)
+}
+
+getBeers()
