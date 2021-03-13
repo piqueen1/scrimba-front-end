02e921f5b490dacdf65709d6c1f0cecb36d6b808
Finn Thye <piqueen@gmail.com>
2021-03-12 19:42:07 -0700

    Fix generic images and grid display

--- mod-7-punk-app/index.js
+++ mod-7-punk-app/index.js
@@ -78,12 +78,13 @@ async function getBeers() {
   const beersDiv = document.querySelector('.beers');
 
   let beerHtml = "";
+  const genericBottle = 'https://cdn.pixabay.com/photo/2014/12/22/00/04/bottle-576717_960_720.png';
 
   beers.forEach(beer => {
     beerHtml += `
     <div class='beer-wrapper card'>
       <div class='beer'>
-        <img class='beer__img' src="${beer.image_url}">
+        <img class='beer__img' src="${beer.image_url ? beer.image_url : genericBottle}">
         <h3>${beer.name}</h3>
         <span class='beer__info'>
           <span>ABV: ${beer.abv}%</span>
