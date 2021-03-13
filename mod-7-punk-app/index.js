d21ef8fb91116002e6b2ba4af728fcf4c80d12e8
Finn Thye <piqueen@gmail.com>
2021-03-12 19:27:24 -0700

    Add pagination

--- mod-7-punk-app/index.js
+++ mod-7-punk-app/index.js
@@ -1,9 +1,11 @@
 // variables
-const urlBase = "https://api.punkapi.com/v2/beers";
+const urlBase = "https://api.punkapi.com/v2/beers?page=";
 const filterABV = document.getElementById('filterABV');
 const filterIBU = document.getElementById('filterIBU');
-let optionsABV = "";
-let optionsIBU = "";
+const pageText = document.getElementById('pageNumber');
+const prevPage = document.getElementById('prevPage');
+const nextPage = document.getElementById('nextPage');
+let optionsABV = "", optionsIBU = "", page = 1;
 
 //filters
 filterABV.addEventListener("change", e=> {
@@ -14,16 +16,17 @@ filterABV.addEventListener("change", e=> {
       optionsABV = "";
       break
     case "weak":
-      optionsABV = "abv_lt=4.6"
+      optionsABV = "&abv_lt=4.6"
       break
     case "medium": 
-      optionsABV = "abv_gt=4.5&abv_lt=7.6";
+      optionsABV = "&abv_gt=4.5&abv_lt=7.6";
       break
     case "strong": 
-      optionsABV = "abv_gt=7.5";
+      optionsABV = "&abv_gt=7.5";
       break
   }
 
+  page = 1;
   getBeers();
 });
 
@@ -35,25 +38,41 @@ filterIBU.addEventListener("change", e=> {
       optionsIBU = "";
       break
     case "weak":
-      optionsIBU = "ibu_lt=35"
+      optionsIBU = "&ibu_lt=35"
       break
     case "medium": 
-      optionsIBU = "ibu_gt=34&ibu_lt=75";
+      optionsIBU = "&ibu_gt=34&ibu_lt=75";
       break
     case "strong": 
-      optionsIBU = "ibu_gt=74";
+      optionsIBU = "&ibu_gt=74";
       break
   }
 
+  page = 1;
   getBeers();
 });
 
 async function getBeers() {
-  const url = urlBase + "?" + optionsABV + "&" + optionsIBU;
+  const url = urlBase + page + optionsABV + optionsIBU;
 
   //fetch
   const promise = await fetch(url);
   const beers = await promise.json();
+
+  //pagination
+  pageText.innerText = page;
+
+  if(page === 1) {
+    prevPage.disabled = true;
+  } else {
+    prevPage.disabled = false;
+  }
+
+  if(beers.length < 25) {
+    nextPage.disabled = true;
+  } else {
+    nextPage.disabled = false;
+  }
 
   // render data
   const beersDiv = document.querySelector('.beers');
@@ -82,8 +101,20 @@ async function getBeers() {
     </div>
     `
   })
-
+  
   beersDiv.innerHTML = beerHtml;
-}
+  }
 
+  //pagination
+prevPage.addEventListener('click', () => {
+  page--;
+  getBeers();
+})
+
+nextPage.addEventListener('click', () => {
+  page++;
+  getBeers();
+})
+
+//initial get
 getBeers()
