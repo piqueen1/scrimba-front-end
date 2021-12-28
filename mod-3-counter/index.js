554705159fd85aabcee8e3fd4bd0df7e7a6c40c5
Finn Thye <piqueen@gmail.com>
2021-12-28 07:16:37 -0700

    Most of counter app coded

--- mod-3-counter/index.js
+++ mod-3-counter/index.js
@@ -1,1 +1,15 @@
-document.getElementById("count-el").innerText = 5
+//document.getElementById("count-el").innerText = 5
+
+let count = 0
+let saveEl = document.getElementById("save-el")
+console.log(saveEl)
+function increment() {
+  count = count + 1
+  document.getElementById("count-el").innerText = count
+  console.log(`The count is now ${count}`)
+}
+
+function save() {
+  let printable = count + " - "
+  saveEl.innerText += printable
+}
