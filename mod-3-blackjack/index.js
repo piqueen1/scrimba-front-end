dae74b91f37761050c2299beae34789af992d2e8
Finn Thye <piqueen@gmail.com>
2021-12-31 06:40:07 -0700

    Create start button and hook up message element to start function

--- mod-3-blackjack/index.js
+++ mod-3-blackjack/index.js
@@ -1,21 +1,19 @@
 let firstCard = 11
-let secondCard = 11
+let secondCard = 9
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
 
-// Write the conditional according to these rules:
+function start() {
+  if (sum < 21) {
+    message = "You're in!  Do you want another card?"
+  } else if (sum === 21) {
+    message = "You got blackjack! :)"
+  } else {
+    message = "You're out of the game :("
+  }
 
-// if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
-// else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
-// else -> "You're out of the game! 😭"
-if (sum < 21) {
-  message = "Do you want another card?"
-} else if (sum === 21) {
-  message = "You got blackjack!"
-} else {
-  message = "You're out of the game 😭"
+  messageEl.textContent = message
 }
-
-console.log(message)
