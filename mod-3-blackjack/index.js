326c38846a9df6c4a9153254ab343f4387d565ad
Finn Thye <piqueen@gmail.com>
2021-12-31 07:16:15 -0700

    Cards and Sum messages displayed

--- mod-3-blackjack/index.js
+++ mod-3-blackjack/index.js
@@ -1,18 +1,26 @@
 let firstCard = 11
-let secondCard = 9
+let secondCard = 10
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
 
 function start() {
+  sumEl.textContent = `Sum: ${sum}`
+  cardsEl.textContent = `Cards: ${firstCard} and ${secondCard}`
+
   if (sum < 21) {
     message = "You're in!  Do you want another card?"
   } else if (sum === 21) {
     message = "You got blackjack! :)"
+    hasBlackJack = true
   } else {
     message = "You're out of the game :("
+    isAlive = false
   }
 
   messageEl.textContent = message
