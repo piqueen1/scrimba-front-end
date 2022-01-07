9230a8e9e6ce0ddd19211e1085b4f0b15ec39a44
Finn Thye <piqueen@gmail.com>
2022-01-07 05:47:56 -0700

    Fix start function

--- mod-3-blackjack/index.js
+++ mod-3-blackjack/index.js
@@ -1,15 +1,16 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function start() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
   cards = [firstCard, secondCard]
   sum = firstCard + secondCard
   renderGame()
