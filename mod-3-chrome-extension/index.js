eede0d93a43b90e7f302b91f51b9bea58eb2273c
Finn Thye <piqueen@gmail.com>
2022-01-10 06:01:06 -0700

    Rewrite as template string

--- mod-3-chrome-extension/index.js
+++ mod-3-chrome-extension/index.js
@@ -14,7 +14,7 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
   listItems = ""
   for (let i=0; i<myLeads.length; i++) {
-    listItems += `<li><a href="${myLeads[i]}">${myLeads[i]}</a></li>`
+    listItems += `<li><a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a></li>`
   }
   ulEl.innerHTML = listItems
 }
